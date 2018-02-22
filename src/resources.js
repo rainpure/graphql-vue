db0320da05cf3a301f7b84269142638a5ab1e96c
cancan <790864964@qq.com>
2018-02-22 13:26:13 +0800

    :sparkles: resources

--- src/resources.js
+++ src/resources.js
@@ -1,6 +1,17 @@
+const debug = process.env.NODE_ENV === 'development' ? true : false;
+// 测试环境地址
+if (debug) {
+    var host = 'http://localhost:8080';
+} else {
+    //正式环境地址
+    var host = 'http://localhost:8080';
+}
+console.warn('当前resource: ', host);
+
 let resources = {
+    host: host,
     // graphQL查询Api
-    graphQlApi: `http://localhost:8080/graphql`
+    graphQlApi: `${host}\/graphql`
 };
 
 export default resources;
