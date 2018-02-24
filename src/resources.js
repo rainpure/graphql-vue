4d5910ec8402aa6a1bc406bad02722eb1fc6ae73
cancan <790864964@qq.com>
2018-02-24 15:37:35 +0800

    :sparkles: 配置本机域名

--- src/resources.js
+++ src/resources.js
@@ -1,10 +1,11 @@
 const debug = process.env.NODE_ENV === 'development' ? true : false;
 // 测试环境地址
 if (debug) {
-    var host = 'http://localhost:8080';
+    // var host = 'http://localhost:8080';
+    var host = 'http://172.16.2.221:8080';
 } else {
     //正式环境地址
-    var host = 'http://localhost:8080';
+    var host = 'http://172.16.2.221:8080';
 }
 console.warn('当前resource: ', host);
 
