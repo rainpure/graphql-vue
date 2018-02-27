06c57ef85e47cc2bcfab329cc9f4adf769f7441b
cancan <790864964@qq.com>
2018-02-27 22:10:12 +0800

    update config

--- src/resources.js
+++ src/resources.js
@@ -2,12 +2,12 @@ const debug = process.env.NODE_ENV === 'development' ? true : false;
 // 测试环境地址
 if (debug) {
     // var host = 'http://localhost:8080';
-    var host = 'http://172.16.2.221:8080'; // 公司
-    // var host = 'http://192.168.1.23:8080'; // 宿舍
+    // var host = 'http://172.16.2.221:8080'; // 公司
+    var host = 'http://192.168.1.23:8080'; // 宿舍
 } else {
     //正式环境地址
-    var host = 'http://172.16.2.221:8080'; // 公司
-    // var host = 'http://192.168.1.23:8080'; // 宿舍
+    // var host = 'http://172.16.2.221:8080'; // 公司
+    var host = 'http://192.168.1.23:8080'; // 宿舍
 }
 console.warn('当前resource: ', host);
 
